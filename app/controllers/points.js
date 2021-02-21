9cb984328b5270ca26c398552a30f8d1993fc9b8
AdelleMcateer <adellemaca@gmail.com>
2021-02-21 10:39:39 +0000

    Changes to app from the introduction of handlebars

--- app/controllers/points.js
+++ app/controllers/points.js
@@ -1,19 +1,21 @@
-const Points = {
+"use strict";
+
+const Donations = {
   index: {
     handler: function (request, h) {
-      return h.file("./app/views/main.html");
+      return h.view("main", { title: "Welcome to Islands of Ireland" });
     },
   },
   signup: {
     handler: function (request, h) {
-      return h.file("./app/views/signup.html");
+      return h.view("signup", { title: "Sign up to add points of interest" });
     },
   },
   login: {
     handler: function (request, h) {
-      return h.file("./app/views/login.html");
+      return h.view("login", { title: "Login to view your account and activity" });
     },
   },
 };
 
-module.exports = Points;
+module.exports = Donations;
