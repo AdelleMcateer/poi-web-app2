944c96d6fa53f887c359a73dcb57b5c794f2ff1c
AdelleMcateer <adellemaca@gmail.com>
2021-02-21 11:51:44 +0000

    Revised routes to serve new/updated controllers. Updated title-card.hbs.

--- routes.js
+++ routes.js
@@ -1,8 +1,26 @@
+"use strict";
+
+const Accounts = require("./app/controllers/accounts");
 const Points = require("./app/controllers/points");
 
 module.exports = [
-  { method: "GET", path: "/", config: Points.index },
-  { method: "GET", path: "/signup", config: Points.signup },
-  { method: "GET", path: "/login", config: Points.login },
-  { method: "GET", path: "/{param*}", handler: { directory: { path: "./public" } } },
+  { method: "GET", path: "/", config: Accounts.index },
+  { method: "GET", path: "/signup", config: Accounts.showSignup },
+  { method: "GET", path: "/login", config: Accounts.showLogin },
+  { method: "GET", path: "/logout", config: Accounts.logout },
+  { method: "POST", path: "/signup", config: Accounts.signup },
+  { method: "POST", path: "/login", config: Accounts.login },
+
+  { method: "GET", path: "/home", config: Points.home },
+  { method: "GET", path: "/report", config: Points.report },
+
+  {
+    method: "GET",
+    path: "/{param*}",
+    handler: {
+      directory: {
+        path: "./public",
+      },
+    },
+  },
 ];
