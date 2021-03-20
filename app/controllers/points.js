d59253f550c170f8e1c53bd56d0544960088e930
AdelleMcateer <adellemaca@gmail.com>
2021-03-20 11:06:32 +0000

    Working on categories.

--- app/controllers/points.js
+++ app/controllers/points.js
@@ -10,24 +10,16 @@ const User = require("../models/user");
 
 const Points = {
   home: {
-    handler: function (request, h) {
-      return h.view("home", { title: "Add an point of interest" });
+    handler: async function (request, h) {
+      const categories = await Category.find().lean();
+      return h.view("home", { title: "Add an point of interest", categories: categories });
     },
   },
-  /*adminhome: {
-    handler: async function (request, h) {
-      const users = await User.find().populate().lean();
-      return h.view("admin-menu", {
-        title: "Admin Home",
-        //categories: categories,
-        users: users,
-      });
-    },
-  },*/
+
   report: {
     handler: async function (request, h) {
       const user = await User.findById(request.auth.credentials.id);
-      const point = await Poi.find().populate("contributor").lean();
+      const point = await Poi.find().populate("contributor").populate("contributor").lean();
       return h.view("report", {
         title: "Points of interest so far",
         point: point,
@@ -39,12 +31,16 @@ const Points = {
       const id = request.auth.credentials.id;
       const user = await User.findById(id);
       const data = request.payload;
+      const rawCategory = request.payload.category;
+      const category = await Category.findOne({
+        name: rawCategory,
+      });
 
       const newPoi = new Poi({
         name: data.name,
         description: data.description,
         contributor: user._id,
-        //category: data.category,
+        category: category._id,
       });
       await newPoi.save();
       return h.redirect("/report");
--- app/models/category.js
+++ app/models/category.js
@@ -5,10 +5,6 @@ const Schema = Mongoose.Schema;
 
 const categorySchema = new Schema({
   categoryName: String,
-  contributor: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
 });
 
 module.exports = Mongoose.model("Category", categorySchema);
